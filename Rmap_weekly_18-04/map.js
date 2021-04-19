ba37d98666e5ba8f902ee437c94e8fcc0f0366b6
bobby-he <bobbyhe@hotmail.co.uk>
2021-04-19 15:31:31 +0100

    movearound

--- Rmap_weekly_18-04/map.js
+++ Rmap_weekly_18-04/map.js
@@ -1,6 +1,6 @@
 // Constants
 const TOPOJSON_PATH = "uk_lad_boundaries.json";
-const MAP_PATH = 'default';
+// const MAP_PATH = 'default';
 const RT_PATH = "Rt.csv";
 const SITE_DATA_PATH = "site_data.csv";
 const CASE_PROJECTION_PATH = "Cproj.csv";
@@ -213,10 +213,10 @@ const loadCases = d3.csv(SITE_DATA_PATH).then(data => {
 });
 
 const urlParams = new URLSearchParams(window.location.search);
-const map_path = urlParams.get('map') || MAP_PATH
-const rt_path = map_path.concat('/', RT_PATH);
-const case_projection_path = map_path.concat('/', CASE_PROJECTION_PATH);
-const case_prediction_path = map_path.concat('/', CASE_PREDICTION_PATH);
+// const map_path = urlParams.get('map') || MAP_PATH
+const rt_path = RT_PATH;
+const case_projection_path = CASE_PROJECTION_PATH;
+const case_prediction_path = CASE_PREDICTION_PATH;
 
 const loadRt = d3.csv(rt_path).then(data => {
     data.forEach(d => {
